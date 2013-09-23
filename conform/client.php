1e76bc9b1572b4d6c9d38408688575ece67e5faf
chuyskywalker <github.com@jrm.cc>
2013-09-22 20:50:14 -0700

    Conformance test including idl files, php server, instructions; output dumps json back-and-forth for better validation of raw transfer, doesn't yet confirm results via expected input format. In reality, a round of unit testing should be employed instead

---
+++ conform/client.php
@@ -0,0 +1,53 @@
+<?php
+require '../vendor/autoload.php';
+
+$inFile  = $argv[1];
+
+$barrister = new \coopernurse\barrister\Barrister();
+$client    = $barrister->httpClient("http://localhost:9233/server.php");
+
+$in = fopen($inFile, "r");
+
+$batch = null;
+
+while (($line = fgets($in)) !== false) {
+
+  $line = trim($line);
+
+  if ($line === "" || $line[0] === "#") {
+    continue;
+  }
+
+  echo "\n -- $line\n";
+
+  if ($line === "start_batch") {
+    $batch = $client->startBatch();
+  }
+  elseif ($line === "end_batch") {
+    $results = $batch->send();
+    foreach ($results as $i => $result) {
+      echo ' -> ' . json_encode($batch->getRequest($i)) . "\n";
+      echo ' <- ' . json_encode($result) . "\n";
+    }
+    $batch = null;
+  }
+  else {
+    $cols = preg_split("/\\|/", $line);
+    $iface     = $cols[0];
+    $func      = $cols[1];
+    $params    = $cols[2];
+    $expStatus = $cols[3];
+    $expResult = $cols[4];
+
+    $paramsNative = json_decode($params);
+    $method = $iface . "." . $func;
+    if ($batch) {
+      $batch->request($method, $paramsNative);
+    }
+    else {
+      echo ' -> ' . json_encode($client->createRequest($method, $paramsNative)) . "\n";
+      echo ' <- ' . json_encode($client->request($method, $paramsNative)) . "\n";
+    }
+  }
+}
+fclose($in);
---
+++ conform/server.php
@@ -0,0 +1,83 @@
+<?php
+require '../vendor/autoload.php';
+
+class A {
+
+  function add($a, $b) {
+    return $a+$b;
+  }
+
+  function calc($nums, $op) {
+    $total = 0;
+    if ($op === "multiply") {
+      $total = 1;
+    }
+
+    foreach ($nums as $i=>$num) {
+      if ($op === "add") {
+        $total += $num;
+      }
+      elseif ($op === "multiply") {
+        $total = $total * $num;
+      }
+      else {
+        throw new Exception("Unknown op: $op");
+      }
+    }
+
+    return $total;
+  }
+
+  function sqrt($a) {
+    return sqrt($a);
+  }
+
+  function repeat($req) {
+    $resp = array("status"=>"ok", "count"=> $req->count, "items"=> array() );
+    $s = $req->to_repeat;
+    if ($req->force_uppercase) {
+      $s = strtoupper($s);
+    }
+
+    for ($i = 0; $i < $req->count; $i++) {
+      array_push($resp["items"], $s);
+    }
+
+    return $resp;
+  }
+
+  function repeat_num($num, $count) {
+    $arr = array();
+    for ($i = 0; $i < $count; $i++) {
+      array_push($arr, $num);
+    }
+    return $arr;
+  }
+
+  function say_hi() {
+    return array("hi"=>"hi");
+  }
+
+  function putPerson($person) {
+    return $person->personId;
+  }
+
+}
+
+class B {
+
+  function echo_($s) {
+    if ($s === "return-null") {
+      return null;
+    }
+    else {
+      return $s;
+    }
+  }
+
+}
+
+$server = new \coopernurse\barrister\Server(__DIR__ . "/conform.json");
+$server->addHandler("A", new A());
+$server->addHandler("B", new B());
+$server->handleHTTP();
